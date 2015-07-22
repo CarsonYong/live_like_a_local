751761fb5558b460f3fac649396812fc069edcd5
Carson Yong <CarsonYong@gmail.com>
2015-07-22 22:31:32 +0000

    Added login/logout feature as well as user personaliztion

--- app/actions.rb
+++ app/actions.rb
@@ -1,17 +1,55 @@
 # Homepage (Root path)
+enable :session
+
 get '/' do
-  erb :index
+  erb :'index'
 end
 
 get '/user/signup' do
   erb :'user/signup'
 end
 
+def set_session_info(user)
+  session['user_id'] = user.id
+  session['first_name'] = user.first_name
+end
+
+def delete_session_info
+  session['user_id'] = nil
+  session['username'] = nil
+end
+
 post '/user/signup' do
   @user = User.new(params[:users])
   if @user.save
+    set_session_info(@user)
     redirect '/'
   else
     erb :'user/signup'
   end
 end
+
+get '/user/login' do
+  erb :'user/login'
+end
+
+post '/user/login' do
+  @user = User.find_by_username(params[:username])
+  if @user == nil
+    @error = 'Username not found!'
+    return erb :'user/login'
+  end
+  if @user.password == (params[:password])
+    set_session_info(@user)
+    redirect '/'
+  else
+    @error = 'Password is incorrect!'
+    erb :'user/login'
+  end
+end
+
+get '/user/logout' do
+  delete_session_info
+  redirect '/'
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /(\w+\.?\w+@\w+\.\w+)/, message: 'is not a valid email'}
-  validates :password, presence: true, length: { minimum: 8 }, format: {with: /^([a-zA-Z0-9@*#_-]{8,25})$/, message: 'is not a valid password'}
+  validates :password, presence: true, length: { minimum: 8 }, format: {with: /([a-zA-Z0-9@*#_-]{8,25})/, message: 'is not a valid password'}
 
 end
