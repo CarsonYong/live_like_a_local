59fc758b9dc2c95ecd884eae10d710cf36cc2a5e
Lianne <lyharrison@sky.com>
2015-07-26 17:29:50 +0000

    commiting before merging with itinerary branch

---
+++ seeds.rb
@@ -0,0 +1,8 @@
+Location.create(country: 'Canada', city: 'Toronto')
+
+Sight.create(sight_name: 'Handlebar', category: 'bars', feature: 'beer', location_id: 1, instagram_location: 214684470)
+Sight.create(sight_name: 'St. Lawrence Market', category: 'cool shit', feature: 'colourful market area', location_id: 1, instagram_location: 54992)
+Sight.create(sight_name: 'Trinity Bellwoods Park', category: 'parks', feature: 'awesome park', location_id: 1, instagram_location: 5824)
+Sight.create(sight_name: 'Aunties & Uncles', category: 'restaurants', feature: 'Brunch', location_id: 1, instagram_location: 152412)
+Sight.create(sight_name: 'Sneaky Dees', category: 'bars', feature: 'Beers and Nachos', location_id: 1, instagram_location: 4201)
+Sight.create(sight_name: 'Bellwoods Brewery', category: 'bars', feature: 'Craft Beers', location_id: 1, instagram_location: 11657414)
